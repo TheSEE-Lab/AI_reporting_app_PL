70a654970cb704ae0d680d702703d910004ed836
Szymek Drobniak <szymek.drobniak@uj.edu.pl>
2026-07-20 14:36:09 +0200

    title mod

--- app.R
+++ app.R
@@ -91,7 +91,10 @@ ui <- fluidPage(
     # Right: options (wider)
     column(
       width = 7,
-      titlePanel("GRONOST<b>AI</b>"),
+      titlePanel(tagList(
+        "GRONOST",
+        tags$strong("AI")
+      )),
   h4("Generator Raportów O Narzędziach i Sposobach wykorzystania AI"),
   p(HTML("System umożliwia wybór ustandaryzowanych przypadków użycia GSI i na podstawie podanych informacji generuje
   wystandaryzowane oświadczenie o ujawnieniu wykorzystania GSI, które można skopiować i wkleić do odpowiedniej sekcji publikacji lub pracy. <br><br>
